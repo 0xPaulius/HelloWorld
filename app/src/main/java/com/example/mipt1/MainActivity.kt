89477f212bee6ae0f10cb144fbacb16b98ca091e
Paulius <turauskaspaulius@gmail.com>
2024-09-15 11:21:07 +0300

    Added button

--- app/src/main/java/com/example/mipt1/MainActivity.kt
+++ app/src/main/java/com/example/mipt1/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mipt1.ui.theme.MIPT1Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MIPT1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainContent(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +33,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainContent(modifier: Modifier = Modifier) {
+    val displayText = remember { mutableStateOf("") }
+
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(text = displayText.value)
+
+        Button(
+            onClick = { displayText.value = "Pirmas darbas" },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Mygtukas")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainContentPreview() {
     MIPT1Theme {
-        Greeting("Android")
+        MainContent()
     }
 }
